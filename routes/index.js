7685760b336f3708d165e465316d88731f010b9a
CamilaApai <camilaapai@hotmail.com>
2023-07-21 16:03:41 -0300

    Swagger Documentation Added

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-
 const controller = require("../controllers")
+
 router.get("/", controller.router)
+router.use('/', require('./swagger'));
 router.use('/contacts', require('./contacts'))
 
 module.exports = router;
