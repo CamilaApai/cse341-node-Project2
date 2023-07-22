9e1ac9a546438173cb96ac5f2def329c318caaf8
CamilaApai <camilaapai@hotmail.com>
2023-07-22 01:29:34 -0300

    Lesson 8, Swagger File Changes

--- swagger.js
+++ swagger.js
@@ -3,7 +3,7 @@ const swaggerAutogen = require('swagger-autogen')();
 const doc = {
   info: {
     title: 'My API',
-    description: 'Description',
+    description: 'API Documentation for the Singers and Albums collections.',
   },
   host: 'cse341-node-project2-h4kd.onrender.com',
   schemes: ['https'],
